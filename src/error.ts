bc609ba559f102cddaf750c0dddf3f5090aad38d
Vikas Potluri <vikaspotluri123.github@gmail.com>
2023-03-19 13:35:26 -0700

    add support for cause in StrategyError

--- src/error.ts
+++ src/error.ts
@@ -1,5 +1,7 @@
 export class StrategyError extends Error {
-	constructor(message: string, public readonly isUserFacing: boolean) {
-		super(message);
+	constructor(message: string, public readonly isUserFacing: boolean, options?: {cause: any}) {
+		// @ts-expect-error Error constructor supports `{cause}` as second param in newer versions, older versions
+		// silently ignore
+		super(message, options);
 	}
 }
