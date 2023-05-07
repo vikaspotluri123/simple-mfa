56c8ef251c5eec7997cdc512a020b21d4add2619
Vikas Potluri <vikaspotluri123.github@gmail.com>
2023-05-07 15:14:08 -0700

    remove defaults strategies from public-api
    - provides a more lightweight implementation for users that don't want to use the default strategies

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,5 +1,2 @@
 export {StorageService} from './storage.js';
 export {StrategyError} from './error.js';
-
-// @TODO: this should be loaded separately
-export {defaultStrategies} from './default-strategies.js';
