19097acd3e0a924b60a0dd4a8c65036e80853d95
Vikas Potluri <vikaspotluri123.github@gmail.com>
2025-06-16 09:11:51 -0700

    update build target to ES2022

--- src/error.ts
+++ src/error.ts
@@ -1,7 +1,5 @@
 export class StrategyError extends Error {
 	constructor(message: string, public readonly isUserFacing: boolean, options?: {cause: any}) {
-		// @ts-expect-error Error constructor supports `{cause}` as second param in newer versions, older versions
-		// silently ignore
 		super(message, options);
 	}
 }
